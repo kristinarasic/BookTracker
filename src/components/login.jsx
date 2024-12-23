1a4fea8197ae424acf8117f25a49b2a12380282a
kristinarasic <34818471+kristinarasic@users.noreply.github.com>
2024-12-23 13:09:43 +0100

    Sign up page style

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import users from '/public/data/users.json'; 
+import users from '/public/data/users.json';
 import '/src/css/login.css';
 
 const Login = () => {
@@ -15,7 +15,7 @@ const Login = () => {
 
         if (user) {
             console.log('Logged in');
-            navigate('/mainPage', { state: { currentUser: user } }); 
+            navigate('/mainPage', { state: { currentUser: user } });
         } else {
             console.log('Invalid credentials');
         }
@@ -23,30 +23,30 @@ const Login = () => {
 
     return (
         <div className="bigContainer">
-        <div className="login-container">
-            <h1 className="loginHeader">Welcome to the book tracker</h1>
-            <h2 className="lgoinHeader2">Log in</h2>
-            <form className="loginForm" onSubmit={eventHandle}>
-                <input
-                    className="input username usernameInput"
-                    type="text"
-                    placeholder="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
-                    className="input password passwordInput"
+            <div className="login-container">
+                <h1 className="loginHeader">Welcome to the book tracker</h1>
+                <h2 className="lgoinHeader2">Log in</h2>
+                <form className="loginForm" onSubmit={eventHandle}>
+                    <input
+                        className="input username usernameInput"
+                        type="text"
+                        placeholder="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <input
+                        className="input password passwordInput"
 
-                    type="password"
-                    placeholder="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
+                        type="password"
+                        placeholder="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                     <button className="submitLogin" type="submit">Log in</button>
-                <p>or <a href="" onClick={() => navigate('/signUp')} id="signUpLink">sign up</a></p>
-            </form>
+                    <p>or <a href="" onClick={() => navigate('/signUp')} id="signUpLink">Sign up</a></p>
+                </form>
             </div>
-            </div>
+        </div>
     );
 };
 
--- src/components/signUp.jsx
+++ src/components/signUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import '/src/css/signUp.css';
 const SignUp = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -15,22 +15,26 @@ const SignUp = () => {
     };
 
     return (
-        <div className="signup-container">
-            <h1>Sign Up</h1>
-            <form onSubmit={handleSignUp}>
+        <div className="signup-container" id="signup-container">
+            <h1 className="loginHeader">Welcome to the book tracker</h1>
+            <h1 id="signup-title">Sign Up</h1>
+            <form onSubmit={handleSignUp} id="signup-form">
                 <input
                     type="text"
                     placeholder="Enter username"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
+                    id="username-input"
                 />
                 <input
                     type="password"
                     placeholder="Enter password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
+                    id="password-input"
                 />
-                <button type="submit">Sign Up</button>
+                <button type="submit" id="signup-button">Sign Up</button>
+                <p>Already have an account?  <a href="" onClick={() => navigate('/')} id="signUpLink">Log in</a></p>
             </form>
         </div>
     );
